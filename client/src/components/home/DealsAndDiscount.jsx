f6cb68345b7ce612a6b40d9956a117d4db43bfbf
Huntertk <taufik.khan65@gmail.com>
2024-04-06 00:40:49 +0530

    Optimed for laptop device

--- client/src/components/home/DealsAndDiscount.jsx
+++ client/src/components/home/DealsAndDiscount.jsx
@@ -1,7 +1,40 @@
 import '../../styles/dealsAndDiscount.scss';
 
+
+const DealsAndDiscountMediumScreen = () => {
+    return (
+      <>
+      <section className='dealsAndDiscountMainMediumSection'>
+          <div className="dealsAndDiscountTitleContainer">
+              <p>Last minute offer for you</p>
+              <h1>Deals & Discount</h1>
+          </div>
+          <div className="dealsAndDiscountMainContainer">
+              <div id="dealsTopContainer" className='dealsAndDiscountCardDesign'>
+                  <h1>Adventures in Japan</h1>
+                  <p>Get an unforgettable experience from each of our trips! Travel with us!</p>
+              </div>
+                <div id='dealsBottomSmallContainerOne' className='dealsAndDiscountCardDesign'>
+                        <h1>Extreme Madagascar</h1>
+                        <p>Enjoy each of our tours!</p>
+                    </div>
+                    <div id='dealsBottomSmallContainerTwo' className='dealsAndDiscountCardDesign'>
+                        <h1>Amazing Colombia</h1>
+                        <p>Enjoy each of our tours!</p>
+                    </div>
+                    <div className="dealsBottomSubContainer dealsAndDiscountCardDesign" id='dealsBottomSubContainer'>
+                    <h1>Aristocratic Lithuania</h1>
+                    <p>Get an unforgettable experience from each of our trips! Travel with us!</p>
+                </div>
+          </div>
+      </section>
+      </>
+    )
+  }
+
 const DealsAndDiscount = () => {
   return (
+    <>
     <section className='dealsAndDiscountMainSection'>
         <div className="dealsAndDiscountTitleContainer">
             <p>Last minute offer for you</p>
@@ -30,6 +63,8 @@ const DealsAndDiscount = () => {
             </div>
         </div>
     </section>
+    <DealsAndDiscountMediumScreen />
+    </>
   )
 }
 
