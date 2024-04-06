22023cd18a9ec26ad523eefd62feac7cd7920ed3
Huntertk <taufik.khan65@gmail.com>
2024-04-06 16:49:55 +0530

    New Video Updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,9 +18,9 @@ const Home = () => {
       <DealsAndDiscount />
       <TourCategories />
       <ProductContainer />
+      <Award />
       <VideoContainer />
       <CountryGrid />
-      <Award />
     </section>
   )
 }
