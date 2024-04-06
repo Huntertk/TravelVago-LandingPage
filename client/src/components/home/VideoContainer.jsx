9d0d7f3c1938b89d470668959ff5758620698779
Huntertk <taufik.khan65@gmail.com>
2024-04-06 14:14:38 +0530

    For mobile version comopleted

--- client/src/components/home/VideoContainer.jsx
+++ client/src/components/home/VideoContainer.jsx
@@ -5,8 +5,8 @@ const VideoContainer = () => {
   return (
     <section className='videoContainerMainSection'>
         <div className="videoContentContainer">
-            <h1>Tour Categories</h1>
-            <p className='subTitle'>Many places for your trip</p>
+            <h1>Watch Our Video</h1>
+            <p className='subTitle'>Take a Tour</p>
             <p className="desc">Everything was seamless. The private guides were interesting and knowledgeable. We felt that they were more than just guides.</p>
         </div>
         <div className="videoWrapper">
