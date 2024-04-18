4800343be23ad1b4ed68528385a793874ba6de73
Huntertk <taufik.khan65@gmail.com>
2024-04-18 22:01:49 +0530

    Awards auto running completed

--- client/src/components/home/Award.jsx
+++ client/src/components/home/Award.jsx
@@ -1,13 +1,21 @@
 import '../../styles/award.scss';
-import awardImg from '../../assets/images/awards.png';
-
+import awardImgOne from '../../assets/images/awards.png';
+import awardImgTwo from '../../assets/images/award.jpg';
+import awardImgThree from '../../assets/images/award2.jpg';
+import Marquee from "react-fast-marquee";
 const Award = () => {
   return (
     <section className='awardMainSection'>
         <h1>Our Achivements</h1>
-        <div className="awardImgContainer">
-            <img src={awardImg} alt="Travelvago" />
-        </div>
+        <Marquee>
+          <div className="awardImgContainer">
+              <img src={awardImgTwo} alt="Travelvago" />
+          </div>
+
+          <div className="awardImgContainer">
+              <img src={awardImgThree} alt="Travelvago" />
+          </div>
+        </Marquee>
     </section>
   )
 }
