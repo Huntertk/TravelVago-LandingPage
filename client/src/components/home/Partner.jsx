f0671fe0f49e565fd49e6c1068bc24e9b97d52e9
Huntertk <taufik.khan65@gmail.com>
2024-04-20 16:53:20 +0530

    Member Section Made Moveable

--- client/src/components/home/Partner.jsx
+++ client/src/components/home/Partner.jsx
@@ -2,15 +2,20 @@ import '../../styles/partner.scss';
 import memberOneImg from '../../assets/images/memberOne.jpg'
 import memberTwoImg from '../../assets/images/memberTwo.jpg'
 import memberThreeImg from '../../assets/images/memberThree.jpg'
+import Marquee from 'react-fast-marquee';
 
 const Partner = () => {
   return (
     <section className='partnerMainContainer'>
         <h1>Accredited Member of</h1>
         <div className="partnerImgContainer">
+        <Marquee
+        speed={100}
+        >
             <img src={memberOneImg} alt="Mazza" />
             <img src={memberTwoImg} alt="PATA" />
             <img src={memberThreeImg} alt="ATTA" />
+        </Marquee>
         </div>
     </section>
   )
