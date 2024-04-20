8d5aa4cb79fe390c07c886b86a9332664ed1998c
Huntertk <taufik.khan65@gmail.com>
2024-04-20 13:14:43 +0530

    Removing Animations

--- client/src/components/home/Product.jsx
+++ client/src/components/home/Product.jsx
@@ -1,27 +1,10 @@
 import '../../styles/product.scss';
-import { CiStopwatch } from "react-icons/ci";
-import {motion} from 'framer-motion';
 
 const Product = ({data}) => {
-  const easeUpLeft = {
-    initial:{
-        x:-100,
-        opacity:0,
-        scale:0,
-    },
-    whileInView:{
-        x:0,
-        opacity: 1,
-        scale:1,
-      },
-      transition:{
-        duration:0.3
-      }
-}
+  
   return (
-    <motion.div 
+    <div 
       className='productCardContainer'
-      {...easeUpLeft}
     >
       <div className="productImgContainer">
         <img src={data.image} alt={data.title} />
@@ -31,13 +14,9 @@ const Product = ({data}) => {
         <p>{data.desc}</p>
         <div className="buttonContainer">
           <button>View Details</button>
-          {/* <div className="duration">
-            <CiStopwatch />
-          <p> {data.duration}</p>
-          </div> */}
         </div>
       </div>
-    </motion.div>
+    </div>
   )
 }
 
