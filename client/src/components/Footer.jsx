9cef1ee1654b017dce53dd9bae4c11bee38754ad
Huntertk <taufik.khan65@gmail.com>
2024-04-22 11:39:53 +0530

    Footer Created for mobile device

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,33 +2,45 @@ import '../styles/footer.scss';
 import logo from '../assets/images/tvagologo.png';
 import { FaInstagram } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='footerMainContainer'>
-        <div className="footerSubContainer">
-            <div className="oneSubContainer">
+    <section className='footerMainSection'>
+        <div className="footerMainContainer">
+            <div className="footerSubConatiner">
                 <div className="logoContainer">
                     <img src={logo} alt="Travelvago" />
                 </div>
-                <div className="contentContainer">
-                    <p>Travelvago Sdn Bhd is a Malaysian-based travel agency. The company operates as both a Destination Management Company (DMC) and an Online Travel Agency.</p>
-                </div>
-                <div className="socialContainer">
+                <p>Travelvago Sdn Bhd is a Malaysian-based travel agency. The company operates as both a Destination Management Company (DMC) and an Online Travel Agency.</p>
+                <div className="socailIcons">
                     <FaInstagram />
                     <FaFacebookSquare />
                 </div>
             </div>
-            <div className="twoSubContainer">
-                <h1>Contact Info</h1>
-                <div className="contactContainer">
-                    <p>+60356124646</p>
-                    <p>Kuala Lumpur, Malaysia</p>
-                    <p>Mon - Sat 8.00 - 18.00 Sunday CLOSED</p>
+            <div className="footerSubConatiner">
+                <p className='footerTitle'>Quick Links</p>
+                <div className="linksContainer">
+                    <Link to="/terms-condition">Terms & Condition</Link>
+                    <Link to="/privacy-policy">Privacy Policy</Link>
+                </div>
+            </div>
+            <div className="footerSubConatiner">
+                <p className='footerTitle'>Need Help</p>
+                <div className="contactDetailsContainer">
+                    <p className='contactTitle'>For General Enquiries</p>
+                    <p>+66657158194</p>
+                </div>
+                <div className="contactDetailsContainer">
+                    <p className='contactTitle'>For Ticketing Enquiries</p>
+                    <p>+66657158194</p>
                 </div>
             </div>
         </div>
-    </div>
+        <div className="copyRightMainContainer">
+            <p>Copyright © 2024 Travelvago</p>
+        </div>
+    </section>
   )
 }
 
