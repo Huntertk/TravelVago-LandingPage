6b406a5527e2165d039c7f0160c25a3e499fc73b
Huntertk <taufik.khan65@gmail.com>
2024-05-21 20:02:36 +0530

    basic layout completed

--- client/src/components/contact/ContactForm.jsx
+++ client/src/components/contact/ContactForm.jsx
@@ -25,50 +25,98 @@ const ContactForm = () => {
         e.preventDefault();
     }
 
+    const monthName = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+
   return (
     <form className='contact-form-main-container' onSubmit={handleFormSubmit}>
         <h1>Contact Us</h1>
-        <input 
-            type="text"
-            name="name"
-            placeholder='Enter your name' 
-            value={data.name}
-            onChange={handleInputData}
-            required
-        />
-        <input
-            type="email"
-            name="email"
-            value={data.email}
-            onChange={handleInputData}
-            placeholder='Enter your email' 
-        />
+        <div className="travel-information-container">
+            <div className="duration-date-container">
+                <label htmlFor="duration">
+                    <h4>Duration of your trip <span>*</span></h4>
+                    <input 
+                    name='duration'
+                    type="number"
+                    placeholder='Duration of your trip'
+                    min={1}
+                    required
+                    />
+                </label>
+                <label htmlFor="duration">
+                    <h4>Your travel date <span>*</span></h4>
+                    <select name="month" required>
+                    {
+                        monthName.map((month) => (
+                            <option value={month} key={month}>{month}</option>
+                        ))
+                    }
+                    </select>
 
-        <PhoneInput
-            placeholder="Enter phone number"
-            value={phone}
-            onChange={setPhone}
-            defaultCountry="MY"
-            required
-      />
-        <input
-        type="text"
-        name="subject"
-        placeholder='subject'
-        onChange={handleInputData}
-        value={data.subject}
-        required
-        />
-        <textarea
-        name="comment"
-        placeholder="comment...."
-        rows={10}
-        value={data.comment}
-        onChange={handleInputData}
-        required
-        >
-        </textarea>
-        <button type="submit">Send</button>
+                    <select name="month" required>
+                    {
+                        [2024,2025,2026,2027].map((year) => (
+                            <option value={year} key={year}>{year}</option>
+                        ))
+                    }
+                    </select>
+                </label>
+            </div>
+            <div className="pax-selection-container">
+                <label htmlFor="pax">
+                    <h4>Number of Adult <span>*</span></h4>
+                    <div className="pax-section-input">
+
+                        <input 
+                        type="number"
+                        name='adult'
+                        placeholder='Adult'
+                        min={1}
+                        required
+                        />
+                        <input 
+                        type="number"
+                        name='child'
+                        placeholder='Child'
+                        min={0}
+                        required
+                        />
+                        <input 
+                        type="number"
+                        name='infant'
+                        placeholder='Infant'
+                        min={0}
+                        required
+                        />
+                    </div>
+                </label>
+            </div>
+            <div className="budget-container">
+                <label htmlFor="">
+                <h4>Your approximated budget <span>*</span></h4>
+                <div className="budget-selection-container">
+                    <select name="budget">
+                        <option value="1200-1500">1200 - 1500/person</option>
+                        <option value="1500-2000">1500 - 2000/person</option>
+                        <option value="Above 2000">Above 2000/person</option>
+                    </select>
+                    <span className='person-text'>MYR/person</span>
+                </div>
+                </label>
+            </div>
+        </div>
     </form>
   )
 }
