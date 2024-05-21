3da0b36ee10548aa1b3896412d35270284c883e4
Huntertk <taufik.khan65@gmail.com>
2024-05-21 15:17:29 +0530

    Input change handler and onSubmit handler created

--- client/src/components/contact/ContactForm.jsx
+++ client/src/components/contact/ContactForm.jsx
@@ -5,19 +5,42 @@ import { useState } from 'react';
 
 
 const ContactForm = () => {
-    const [phone, setPhone] = useState()
+    const [data, setData] = useState({
+        name:"",
+        email:"",
+        subject:"",
+        comment:""
+    }) 
+    const [phone, setPhone] = useState();
+    const handleInputData = (e) => {
+        setData(prev =>  (
+            {
+                ...prev,
+                [e.target.name]:e.target.value
+            }
+        ))
+    }
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+    }
+
   return (
-    <form className='contact-form-main-container'>
+    <form className='contact-form-main-container' onSubmit={handleFormSubmit}>
         <h1>Contact Us</h1>
         <input 
             type="text"
             name="name"
             placeholder='Enter your name' 
+            value={data.name}
+            onChange={handleInputData}
             required
         />
         <input
             type="email"
             name="email"
+            value={data.email}
+            onChange={handleInputData}
             placeholder='Enter your email' 
         />
 
@@ -32,12 +55,16 @@ const ContactForm = () => {
         type="text"
         name="subject"
         placeholder='subject'
+        onChange={handleInputData}
+        value={data.subject}
         required
         />
         <textarea
         name="comment"
         placeholder="comment...."
         rows={10}
+        value={data.comment}
+        onChange={handleInputData}
         required
         >
         </textarea>
