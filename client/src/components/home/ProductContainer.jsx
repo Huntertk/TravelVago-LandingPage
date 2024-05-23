2613dba6508e652b45ca1f3d8adc5f4317efafa6
Huntertk <taufik.khan65@gmail.com>
2024-05-24 01:46:36 +0530

    removing scrolling when array size less than 3

--- client/src/components/home/ProductContainer.jsx
+++ client/src/components/home/ProductContainer.jsx
@@ -7,7 +7,8 @@ const ProductContainer = ({prodData, para, title}) => {
     <section className='productContainerMainSection'>
       <TitleContainer para={para} title={title}  />
       <div className="productContainer" style={{
-        gridTemplateColumns: `repeat(${prodData.length}, 1fr)`
+        gridTemplateColumns: `repeat(${prodData.length}, 1fr)`,
+        overflowX: prodData.length <= 3 ? "unset" : "scroll" 
       }}>
         {
           prodData.map((data) => <Product key={data.id} data={data}  />)
