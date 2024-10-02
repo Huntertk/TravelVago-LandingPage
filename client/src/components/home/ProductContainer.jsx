6e4883b0c63a766063d5b9469069506cb7a28b6f
Huntertk <taufik.khan65@gmail.com>
2024-10-02 13:16:47 +0530

    inhancing ui

--- client/src/components/home/ProductContainer.jsx
+++ client/src/components/home/ProductContainer.jsx
@@ -1,12 +1,37 @@
+import { useRef } from 'react';
 import '../../styles/productContainer.scss';
 import Product from './Product';
 import TitleContainer from './TitleContainer';
+import { FaCircleChevronLeft, FaCircleChevronRight } from 'react-icons/fa6';
 
 const ProductContainer = ({prodData, para, title}) => {
+  const containerRef = useRef(null);
+  const scrollLeft = () => {
+    if(containerRef.current){
+        containerRef.current.scrollBy({left:-353}) // Adjust the scroll amount
+    }
+  };
+  const scrollRight = () => {
+    if(containerRef.current){
+        containerRef.current.scrollBy({left:353}) // Adjust the scroll amount
+    }
+  };
   return (
     <section className='productContainerMainSection'>
       <TitleContainer para={para} title={title}  />
-      <div className="productContainer" style={{
+      {
+        prodData.length >= 3 && (
+          <>
+            <button className='scrollLeftBtn' onClick={scrollLeft}>
+                <FaCircleChevronLeft />
+              </button>
+              <button className='scrollRightBtn' onClick={scrollRight}>
+                <FaCircleChevronRight />
+              </button>
+          </>
+        ) 
+      }
+      <div className="productContainer" ref={containerRef} style={{
         gridTemplateColumns: `repeat(${prodData.length}, 1fr)`,
         overflowX: prodData.length <= 3 ? "unset" : "scroll" 
       }}>
